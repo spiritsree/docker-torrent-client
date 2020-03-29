251b90b55027c98072461e650b4c3f90371afa32
Sreejith G <spiritsree@192-168-1-104.tpgi.com.au>
2020-03-29 11:30:44 +1100

    removing extra space

--- setup.sh
+++ setup.sh
@@ -5,24 +5,24 @@ IMAGE_OS='ubuntu'
 
 docker build --no-cache -t "${IMAGE_TAG}-${IMAGE_OS}" -f Dockerfile.${IMAGE_OS} app
 
-OPT="--cap-add=NET_ADMIN -d \\ \n"
+OPT="--cap-add=NET_ADMIN -d \\\n"
 # Check if Docker IPv6 is enabled
 ipv6_enabled=$(docker network ls --filter Driver="bridge"  --format "{{.IPv6}}")
 
 # Disable IPv6 if Docker doesn't support it
 if [[ "${ipv6_enabled}" == "false" ]]; then
-    OPT+="\t--sysctl net.ipv6.conf.all.disable_ipv6=0 \\ \n"
+    OPT+="\t--sysctl net.ipv6.conf.all.disable_ipv6=0 \\\n"
 fi
 
 # DNS IPs
-OPT+="\t\t--dns 8.8.8.8 \\ \n \t--dns 8.8.4.4 \\ \n"
+OPT+="\t\t--dns 8.8.8.8 \\\n \t--dns 8.8.4.4 \\\n"
 
 # Volume mount for Data
-OPT+="\t\t-v ~/Downloads/uTorrent/data/:/data \\ \n"
+OPT+="\t\t-v ~/Downloads/uTorrent/data/:/data \\\n"
 
 # OpenVPN username and password
-OPT+="\t\t-e OPENVPN_USERNAME=<username> \\ \n"
-OPT+="\t\t-e OPENVPN_PASSWORD=<password> \\ \n"
+OPT+="\t\t-e OPENVPN_USERNAME=<username> \\\n"
+OPT+="\t\t-e OPENVPN_PASSWORD=<password> \\\n"
 
 # Docker Image to run
 OPT+="\t\t${IMAGE_TAG}-${IMAGE_OS}:latest \n"
